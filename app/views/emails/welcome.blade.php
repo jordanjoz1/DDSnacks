58936b09b9431711df362ddbe2f0bfdcfc3b019c
Jordan Jozwiak <jordanjoz1@gmail.com>
2014-09-23 23:56:26 -0700

    send emails and signup without email

---
+++ app/views/emails/welcome.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<p>Welcome to DD Snacks!</p>
+<p>Your password is: {{ $password }}</p>
+</body>
+</html>
--- app/views/pages/signup.blade.php
+++ app/views/pages/signup.blade.php
@@ -13,22 +13,11 @@
             <!-- if there are login errors, show them here -->
             <p>
                 {{ $errors->first('email') }}
-                {{ $errors->first('password') }}
-                {{ $errors->first('password2') }}
             </p>
 
             <p>
                 {{ Form::label('email', 'Email Address') }}
                 {{ Form::text('email', Input::old('email'), array('placeholder' => 'you@doubledutch.me')) }}
-            </p>
-
-            <p>
-                {{ Form::label('password', 'Password') }}
-                {{ Form::password('password') }}
-            </p>
-            <p>
-                {{ Form::label('password2', 'Confirm password') }}
-                {{ Form::password('password2') }}
             </p>
 
             <p>{{ Form::submit('Submit!') }}</p>
