66f13020153e0785e3fc30bd9447b95ccabd8633
Jordan Jozwiak <jordanjoz1@gmail.com>
2015-05-18 02:14:59 -0700

    change db url constant

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => parse_url(getenv("DATABASE_URL"))["host"],
-			'database'  => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-			'username'  => parse_url(getenv("DATABASE_URL"))["user"],
-			'password'  => parse_url(getenv("DATABASE_URL"))["pass"],
+			'host'      => parse_url(getenv("CLEARDB_DATABASE_URL"))["host"],
+			'database'  => substr(parse_url(getenv("CLEARDB_DATABASE_URL"))["path"], 1),
+			'username'  => parse_url(getenv("CLEARDB_DATABASE_URL"))["user"],
+			'password'  => parse_url(getenv("CLEARDB_DATABASE_URL"))["pass"],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
